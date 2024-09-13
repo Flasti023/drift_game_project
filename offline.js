5e0e3acd038bbd7f928ef7ce120123902a84967d
Flasti023 <155763787+Flasti023@users.noreply.github.com>
2024-09-13 20:47:45 +0400

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1726245451,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ad6c657f6d85c286b787a89ac00a5dbc-sheet0.png",
+		"images/924cf94cd87c2-sheet0.png",
+		"images/машина-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/924cf94cd87c3-sheet0.png",
+		"images/924cf94cd87c4-sheet0.png",
+		"images/924cf94cd87c5-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
